98ad4bdce3ad17d6db2caea9bd8b44653bf33269
Meints <cadumeints0@gmail.com>
2025-03-07 21:44:46 -0300

    adding product navigation and starting detail page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { useKeenSlider } from 'keen-slider/react'
+import Link from "next/link";
 
 import { HomeContainer, Product } from "../styles/pages/home";
 
@@ -28,13 +29,16 @@ export default function Home({ products }: Readonly<HomeProps>) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => (
-        <Product key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} width={520} height={480} alt="" />
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>  
+        <Link key={product.id} href={`/product/${product.id}`}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>  
+        </Link>
+
       ))}
     </HomeContainer>
   )
